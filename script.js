20edccd682d97614aa7da38ba6a9706abf59cf85
Adarsh M M <adarshakashmm@gmail.com>
2024-06-24 20:35:48 +0530

    Adding storage options

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
-const startBtn=document.querySelector(".timer-start");
-const stopBtn=document.querySelector(".timer-stop");
-const restartBtn=document.querySelector(".timer-reset");
-let minutes=document.querySelector(".minutes");
-let seconds=document.querySelector(".seconds");
+const startBtn = document.querySelector(".timer-start");
+const stopBtn = document.querySelector(".timer-stop");
+const restartBtn = document.querySelector(".timer-reset");
+let minutes = document.querySelector(".minutes");
+let seconds = document.querySelector(".seconds");
+let i = 0;
 
-startBtn.addEventListener("click",start);
+minutes.textContent = localStorage.getItem("minutes");
+seconds.textContent = localStorage.getItem("seconds");
 
-stopBtn.addEventListener("click",stop);
+startBtn.addEventListener("click", start);
+
+stopBtn.addEventListener("click", stop);
 
 restartBtn.addEventListener("click", restart);
 
 function start() {
     interval = setInterval(() => {
-        // seconds.textContent=seconds.textContent.toString().padStart(2,"0");
+
         if (seconds.textContent > 0 && seconds.textContent < 60) {
-            seconds.textContent--;
+            // seconds.textContent--;
+            seconds.textContent = (seconds.textContent - 1).toString().padStart(2, "0");
         }
         else if (seconds.textContent == 0 && minutes.textContent != 0) {
             seconds.textContent = 59;
-            minutes.textContent--;
+            // minutes.textContent--;
+            minutes.textContent = (minutes.textContent - 1).toString().padStart(2, "0");
         }
         if (minutes.textContent == 0 && seconds.textContent <= 0) {
             seconds.textContent = 0;
@@ -28,19 +34,40 @@ function start() {
     startBtn.setAttribute("disabled", "disabled");
 }
 
-function stop(){
-        clearInterval(interval);
-        startBtn.removeAttribute("disabled","disabled");
-    }
+function stop() {
+    clearInterval(interval);
+    startBtn.removeAttribute("disabled", "disabled");
+}
 
-function restart(){
-        minutes.textContent= 25;
-        seconds.textContent= 0;
-        stop();
+function restart() {
+    minutes.textContent = 25;
+    seconds.textContent = 0;
+    seconds.textContent = seconds.textContent.toString().padStart(2, "0");
+    stop();
+    localStorage.clear()
 }
 
 function completed() {
     setTimeout(() => (alert("Timer completed..!")), 1000);
     restart();
+}
+
+window.addEventListener('blur', stor(i));
+
+// console.log(window.Notification.requestPermission);
+// window.addEventListener('load', Notification.requestPermission());
+// console.log((Notification.permission));
+// options={
+//     body:"Please enter your name",
+// }
+// // new Notification("Adarsh M M here..!",options);
+
+function stor(iterate) {
+    if (iterate == 0) {
+        start();
+    }
+    localStorage.setItem("minutes", minutes.textContent);
+    localStorage.setItem("seconds", seconds.textContent);
+    i++;
 }
 
